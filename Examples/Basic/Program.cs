d8c2ac1a6214390cfe3fdbc32a2e6ada9588a6ca
martinmolin <martin@ternacode.com>
2021-08-03 13:35:47 +0200

    Restructured examples and set up launch/build tasks for vscode.

---
+++ Examples/Basic/Program.cs
@@ -0,0 +1,19 @@
+﻿using Skytable.Client;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect to Skytable
+            var connection = new Connection("127.0.0.1", 2003);
+
+            // Set the Key 'Basic' to "MyValue"
+            var setResponse = connection.Set("Basic", "MyValue");
+
+            // Get the value "MyValue" of the Key 'Basic'.
+            var getResponse = connection.Get("Basic");
+        }
+    }
+}
---
+++ Examples/Skyhash/Program.cs
@@ -0,0 +1,33 @@
+﻿using Skytable.Client;
+
+namespace SkyhashExample
+{
+    // Inherit from Skyhash to automatically implement From/Into JSON for your type.
+    // From will be used when calling Connection.Set. Default implementation is JSON.
+    // Into will be used when calling Connection.Get. Default implementation is JSON.
+    // You may override From and Into in order to change the storage format of this type.
+    class Person : Skyhash
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setPerson = new Person();
+            setPerson.Name = "John Doe";
+            setPerson.Age = 30;
+
+            // Connect to Skytable
+            var connection = new Connection("127.0.0.1", 2003);
+
+            // Set the Key 'Person' to a JSON string representing the 'setPerson' object.
+            var setResponse = connection.Set("Person", setPerson);
+
+            // 'person' now contains a Person with the name "John Doe" and age 30.
+            var person = connection.Get<Person>("Person");
+        }
+    }
+}
